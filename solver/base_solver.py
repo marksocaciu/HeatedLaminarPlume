fd6d4da897b8b85d47f2a00bda7e16fc63c42360
marksocaciu-lgtm <mark.socaciu@gmail.com>
2026-05-12 09:51:33 +0200

    steady remesh

--- solver/base_solver.py
+++ solver/base_solver.py
@@ -2183,12 +2183,12 @@ def solve_steady_from_loaded_checkpoint(
     )
     k_air = fenics.Constant(experiment.fluid.properties["k"])
     q_heat, q_mag = compute_heat_flux_dim(T_dim, k_air)
-    q_out = T_path.split(".xdmf")[0] + f"_heatflux_transient_{step:05d}.xdmf"
-    qmag_out = T_path.split(".xdmf")[0] + f"_heatflux_mag_transient_{step:05d}.xdmf"
+    q_out = T_path.split(".xdmf")[0] + f"_heatflux_final_steady_{step:05d}.xdmf"
+    qmag_out = T_path.split(".xdmf")[0] + f"_heatflux_mag_final_steady_{step:05d}.xdmf"
 
-    p_out = p_path.split(".xdmf")[0] + f"_transient_{step:05d}.xdmf"
-    u_out = u_path.split(".xdmf")[0] + f"_transient_{step:05d}.xdmf"
-    t_out = T_path.split(".xdmf")[0] + f"_transient_{step:05d}.xdmf"
+    p_out = p_path.split(".xdmf")[0] + f"_final_steady_{step:05d}.xdmf"
+    u_out = u_path.split(".xdmf")[0] + f"_final_steady_{step:05d}.xdmf"
+    t_out = T_path.split(".xdmf")[0] + f"_final_steady_{step:05d}.xdmf"
 
     # --- Effective Grashof number diagnostic ---
     theta_max = float(global_vec_max(theta))
