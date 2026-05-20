171ed633154a97314d5f7eb270ae7a6fdcc51162
marksocaciu-lgtm <mark.socaciu@gmail.com>
2026-05-20 22:44:10 +0200

    rst

--- solver/base_solver.py
+++ solver/base_solver.py
@@ -1479,7 +1479,7 @@ def run_post_continuation_transient(
     restart_settle_steps = 10
     restart_rel_update_reject = 5.0e-2
     restart_rel_u_reject = 2.5e-1
-    restart_rel_theta_reject = 1.0e-1
+    restart_rel_theta_reject = 1.0e0
     restart_u_abs_max = 1.0e3
     restart_theta_min = -1.0
     restart_theta_max = 20.0
