18564269d11baf73eb66c1da700a1f26df7e7e1f
marksocaciu-lgtm <mark.socaciu@gmail.com>
2026-06-18 15:59:00 +0300

    post

--- plt.py
+++ plt.py
@@ -2,8 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Data
-height = np.array([5e-3, 1e-2, 2e-2, 3e-2])
-enthalpy_flow = np.array([2.678796e-01, 3.080905e-01, 3.802539e-01, 4.472166e-01])
+height = np.array([5e-3, 1e-2, 1.5e-2, 2e-2, 2.5e-2, 3e-2, 3.5e-2])
+enthalpy_flow = np.array([2.678796e-01, 3.080905e-01, 3.437198e-01, 3.802539e-01, 4.157289e-01, 4.472166e-01, 4.724517e-01])
 
 # Supplied heating power
 Q_supplied = 0.2697
